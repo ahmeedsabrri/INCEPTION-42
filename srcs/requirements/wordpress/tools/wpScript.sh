619fb881a7f15c19c8b8ce6f3328f8e6fd31ae6c
Ahmed Sabri <asabri@e3r10p2.1337.ma>
2024-03-25 03:18:54 +0000

    done

--- srcs/requirements/wordpress/tools/wpScript.sh
+++ srcs/requirements/wordpress/tools/wpScript.sh
@@ -12,12 +12,13 @@ chmod +x /usr/local/bin/wp
 
 sed -i "s|listen = /run/php/php7.4-fpm.sock|listen = 0.0.0.0:9000|1" /etc/php/7.4/fpm/pool.d/www.conf
 
-service php7.4-fpm start
 
 wp core download --allow-root --force
 wp config create --allow-root --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_USER_PASS" --dbhost="$DB_HOST"
 wp core install --allow-root --url="$URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASS" --admin_email="$WP_ADMIN_MAIL" 
 wp user create   --allow-root "$WP_USER" "$WP_USER_MAIL" --user_pass="$WP_USER_PASS" --role=author
 
+service php7.4-fpm start
 service php7.4-fpm stop
+
 php-fpm7.4 -F
